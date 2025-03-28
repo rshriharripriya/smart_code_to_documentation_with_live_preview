49050d7f6d426dd33f9eb70a67638d52a04a00b7
rshri <rshriharripriya19@gmail.com>
2025-03-27 17:27:01 -0700

    trying to fix the error which causes the prod to not detect code changes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_GIT_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA || '',
-    NEXT_PUBLIC_GIT_BRANCH: process.env.VERCEL_GIT_REPO_BRANCH || 'local',
+    NEXT_PUBLIC_GIT_BRANCH: process.env.main || 'local',
   }
 
   // Add other custom configurations below
